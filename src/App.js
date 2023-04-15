a833cbfe51a1089d8c5a52b04582ad499a02c12e
Samarjeet09 <singhsamarjeet09@gmail.com>
2023-04-16 02:09:21 +0530

    Squashed commit of the following:
    commit a2507a74469bf2f9de5f0f87a85e37fe6af730e0
    Author: Samarjeet09 <singhsamarjeet09@gmail.com>
    Date:   Sun Apr 16 02:00:16 2023 +0530
    
        Squashed commit of the following:
    
        commit a1ed57e77b3635d12132f5c99ff1726371eac790
        Author: Samarjeet09 <singhsamarjeet09@gmail.com>
        Date:   Sun Apr 16 01:39:57 2023 +0530
    
            frontend
    
    commit 6e9ed96fd1590c140a4ba0afb10f1ce230ada83f
    Author: CHIRAG <cgupta_be21@thapar.edu>
    Date:   Sun Apr 16 01:04:36 2023 +0530
    
        lla
    
    commit 793bfa1a0fe867fe5bb26d0d3be5453b96702100
    Author: CHIRAG <cgupta_be21@thapar.edu>
    Date:   Sun Apr 16 00:36:58 2023 +0530
    
        new
    
    commit b4a2e8cc07e964f1b1afad59276e7428d47b4e94
    Author: CHIRAG <cgupta_be21@thapar.edu>
    Date:   Sun Apr 16 00:36:39 2023 +0530
    
        after creating new server
    
    commit 224cc77e444e76b768fc7456ed9aeca9d0f3809b
    Author: CHIRAG <cgupta_be21@thapar.edu>
    Date:   Sat Apr 15 22:49:32 2023 +0530
    
        jlkj
    
    commit 1e977392aeb73af634a25edfe280236e26eec96d
    Author: CHIRAG <cgupta_be21@thapar.edu>
    Date:   Sat Apr 15 22:49:07 2023 +0530
    
        login almost ready

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from "react"
-import Tile from './components/tiles/tiles'
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+
+import Login from './components/Login'
+import SignUp from './components/Signup'
+import UserDetails from './components/userDeatils'
 
 function App() {
 
@@ -18,11 +21,43 @@ function App() {
   };
 
   return (
-    <div style={styles}>
-       <Navbar/>
-       <Tile/>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={'/sign-in'}>
+              StayEz
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-in'}>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-up'}>
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/" element={<Login />} />
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/userDetails" element={<UserDetails />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
